698715bc8ddb9c37a7c3e25c639a20ea1af6cac7
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-05-11 17:57:04 +0200

    Implementation of test layer and test case testing to get product use case by id

--- src/app/infrastructure/repositories/category.repository.ts
+++ src/app/infrastructure/repositories/category.repository.ts
@@ -7,35 +7,35 @@ export class CategoryRepository implements ICategoryRepository {
         {
             id: 1,
             name: "Headphones",
-            image: "assets/img/product/category/product-cat-1.png",
+            image: "assets/img/product/product-cat-1.png",
             link: "shop-category.html",
             product_count: 20
         },
         {
             id: 2,
             name: "Mobile Phone",
-            image: "assets/img/product/category/product-cat-2.png",
+            image: "assets/img/product/product-cat-2.png",
             link: "shop-category1.html",
             product_count: 25
         },
         {
             id: 3,
             name: "CPU Heat Pipes",
-            image: "assets/img/product/category/product-cat-3.png",
+            image: "assets/img/product/product-cat-3.png",
             link: "shop-category.html",
             product_count: 57
         },
         {
             id: 4,
             name: "Smart Watch",
-            image: "assets/img/product/category/product-cat-4.png",
+            image: "assets/img/product/product-cat-4.png",
             link: "shop-category.html",
             product_count: 44
         },
         {
             id: 5,
             name: "With Bluetooth",
-            image: "assets/img/product/category/product-cat-5.png",
+            image: "assets/img/product/product-cat-5.png",
             link: "shop-category.html",
             product_count: 81
         }
---
+++ src/tests/unit-tests/product/get-product-by-id.spec.ts
@@ -0,0 +1,35 @@
+import { GetProductByIdQueryHandler } from "src/app/application/product/queries/get-product-by-id/get-product-by-id.query-handler";
+import { Product } from "src/app/domain/product/product.model";
+import { IProductRepository } from "src/app/domain/product/product.repository";
+
+describe('GetProductByIdQueryHandler', () => {
+    let productRepository: jasmine.SpyObj<IProductRepository>;
+    let getProductByIdQueryHandler: GetProductByIdQueryHandler;
+  
+    beforeEach(() => {
+      productRepository = jasmine.createSpyObj('IProductRepository', ['getProductById']);
+      getProductByIdQueryHandler = new GetProductByIdQueryHandler(productRepository);
+    });
+  
+    it('should return the correct product', async () => {
+      const expectedProduct: Product = {
+        id: 1,
+        name: 'Test Product',
+        price: 10.99,
+        category: '',
+        description: '',
+        images : [],
+        rating: 4,
+        status: ''
+      };
+  
+      // Configure the mock to return the expected product
+      productRepository.getProductById.and.resolveTo(expectedProduct);
+  
+      // Call the execute method with a test product ID
+      const result = await getProductByIdQueryHandler.execute(1);
+  
+      // Expect the result to be the expected product
+      expect(result).toEqual(expectedProduct);
+    });
+  });
