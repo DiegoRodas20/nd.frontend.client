25b900fe0bc0268a980f5598db9c63ba04a79e53
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-05-19 23:51:51 -0500

    Removed module federation library with build changes

--- src/bootstrap.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PresentationModule } from './app/presentation/presentation.module';
-
-platformBrowserDynamic()
-    .bootstrapModule(PresentationModule)
-    .catch(err => console.error(err));
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-import('./bootstrap').catch(err => console.error(err));
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { PresentationModule } from './app/presentation/presentation.module';
+
+platformBrowserDynamic()
+    .bootstrapModule(PresentationModule)
+    .catch(err => console.error(err));
+
