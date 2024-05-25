64046ff5650223336e0089210fd489d978c5aea5
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-05-25 10:40:48 -0500

    Updated shared components

---
+++ src/app/shared/components/badge/badge.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-badge',
+    templateUrl: 'badge.component.html',
+    styleUrls: ['badge.component.scss']
+})
+
+export class BadgeComponent implements OnInit {
+
+    ngOnInit(): void { }
+
+}
