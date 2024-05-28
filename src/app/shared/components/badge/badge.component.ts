40b7b6ea4dca9d8310d0f941aa8aa09f8883f169
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-05-27 22:40:49 -0500

    Badge component implemented

--- src/app/shared/components/badge/badge.component.ts
+++ src/app/shared/components/badge/badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-badge',
@@ -7,6 +7,8 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class BadgeComponent implements OnInit {
+
+    @Input() status: string = ""
 
     ngOnInit(): void { }
 
